3d49c49f6e0796f7b74f99a85928c9cbeebfc65c
Raphael Fialho <rapguit@gmail.com>
2018-12-11 23:42:54 -0200

    o poc implemetado

---
+++ src/main/java/com/zoop/maroliar/MaroliarApplication.java
@@ -0,0 +1,12 @@
+package com.zoop.maroliar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaroliarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaroliarApplication.class, args);
+	}
+}
